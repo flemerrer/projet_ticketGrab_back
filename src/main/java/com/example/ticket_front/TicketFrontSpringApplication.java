9a7b47087087fd189642cdea3e0ab7a86820c051
flemerrer <francois.lemerrer@pm.me>
2023-09-07 19:29:19 +0200

    setup

---
+++ src/main/java/com/example/ticket_front/TicketFrontSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticket_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketFrontSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketFrontSpringApplication.class, args);
+    }
+
+}
