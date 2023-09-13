a3add20ae6943a0a0e9eb9665656c512fb953261
Al Hct <albinhuchet@gmail.com>
2023-09-13 10:13:04 +0000

    changements feature_connexion

--- src/main/java/com/example/ticket_front/service/UserService.java
+++ src/main/java/com/example/ticket_front/service/UserService.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.List;
 
