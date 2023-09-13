ccbf9323b2326e59239d9cf2f2b53c7c9797e4d2
Al Hct <albinhuchet@gmail.com>
2023-09-13 15:43:15 +0000

    UserController, crud pratiquement fini

--- src/main/java/com/example/ticket_api/services/UserService.java
+++ src/main/java/com/example/ticket_api/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.ticket_api.services;
 
 import com.example.ticket_api.entities.User;
+import com.example.ticket_api.entities.dto.UserDto;
 import com.example.ticket_api.repositories.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -29,6 +30,24 @@ public class UserService {
             return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(null);
         }
     }
+
+    public ResponseEntity<String> deleteUser (User user) {
+        if (user.getFirstName() != null && user.getLastName() != null && user.getEmail() != null && user.getPassword() != null) {
+            userRepo.delete(user);
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(null);
+        }
+    }
+    public ResponseEntity<String> deleteAllUsers () {
+        userRepo.deleteAll();
+        List<User> users = userRepo.findAll();
+        if (users.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.ACCEPTED).body(null); // Retourne un code NO CONTENT si l'objet est vide.
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(null);
+        }
+    }
     public ResponseEntity<List<User>> findAllUsers (Model model) {
         List<User> users = userRepo.findAll();
         if (users.isEmpty()) {
@@ -37,59 +56,62 @@ public class UserService {
             return ResponseEntity.ok(users);
         }
     }
+    public ResponseEntity<User> findUserById (Long id) {
+        User u=userRepo.findUserById(id);
+        if (u!=null){
+            return ResponseEntity.ok(u);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-    public ResponseEntity<User> updateUser (User userToUpdate) {
-        User user = userRepo.findUserByEmailAndPassword(userToUpdate.getEmail(), userToUpdate.getLastName());
-        if (user!=null) {
-            user.setEmail(user.getEmail());
-            user.setPassword(user.getPassword());
-            user.setFirstName(user.getFirstName());
-            user.setLastName(user.getLastName());
-            user.setSoldTicketsNumber(user.getSoldTicketsNumber());
-            user.setAdmin(user.getAdmin());
+    public ResponseEntity<User> updatePassword (UserDto userToUpdate) {
+        User user = userRepo.findUserByEmailAndPassword(userToUpdate.getEmail(), userToUpdate.getPassword());
+        if (user != null) {
+            user.setPassword(userToUpdate.getPassword02());
             userRepo.save(user);
             return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
         } else {
             return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(null);
         }
     }
-
-    public ResponseEntity<User> login (Model model, String emailUser, String passwordUser) {
-        User u = userRepo.findUserByEmailAndPassword(emailUser, passwordUser);
-        if (u==null) {
-            System.out.println("utilisateur non trouvé");
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(null); // Retourne un code NO CONTENT si l'objet est vide.
-        } else {
-            System.out.println("utilisateur trouvé, connexion en cours");
-            HttpServletRequest request =null;
-            HttpSession httpSession = request.getSession();
-            httpSession.setAttribute("username", u.getFirstName());
-            return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
+        public ResponseEntity<User> updateUser (User userToUpdate) {
+            User user = userRepo.findUserByEmailAndPassword(userToUpdate.getEmail(), userToUpdate.getPassword());
+            if (user!=null) {
+                user.setEmail(userToUpdate.getEmail());
+                user.setPassword(userToUpdate.getPassword());
+                user.setFirstName(userToUpdate.getFirstName());
+                user.setLastName(userToUpdate.getLastName());
+                if (userToUpdate.getSoldTicketsNumber()!=null) {
+                    user.setSoldTicketsNumber(userToUpdate.getSoldTicketsNumber());
+                }
+                if (userToUpdate.getAdmin()!=null) {
+                    user.setAdmin(userToUpdate.getAdmin());
+                }
+                userRepo.save(user);
+                return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(null);
+            }
         }
-    }
 
-    public String logoff (HttpSession httpSession, String username) {
-        System.out.println("Déconnexion en cours");
-        httpSession.invalidate();
-        return "Déconnexion réussie";
-    }
-
-
-//Méthodes de controller, pas de rest controller
-
-//    public String showForm(Model model) {
-//        model.addAttribute("user", new User());
-//        return "create-user"; // Le nom de la vue du formulaire de création
-//    }
-//    public String submitForm(@ModelAttribute User user, Model model) {
-//        model.addAttribute("user",  user);
-//        userRepo.save(user);
-//        return this.findAllUsers(model); // Le nom de la vue du formulaire de création
-//    }
+//        public ResponseEntity<User> login (Model model, String emailUser, String passwordUser) {
+//            User u = userRepo.findUserByEmailAndPassword(emailUser, passwordUser);
+//            if (u==null) {
+//                System.out.println("utilisateur non trouvé");
+//                return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(null); // Retourne un code NO CONTENT si l'objet est vide.
+//            } else {
+//                System.out.println("utilisateur trouvé, connexion en cours");
+//                HttpServletRequest request =null;
+//                HttpSession httpSession = request.getSession();
+//                httpSession.setAttribute("username", u.getFirstName());
+//                return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
+//            }
+//        }
 //
-//    public String findAllUsers(Model model) {
-//        List<User> users = userRepo.findAll();
-//        model.addAttribute("users", users);
-//        return "users";
-//    }
-}
+//        public String logoff (HttpSession httpSession, String username) {
+//            System.out.println("Déconnexion en cours");
+//            httpSession.invalidate();
+//            return "Déconnexion réussie";
+//        }
+    }
